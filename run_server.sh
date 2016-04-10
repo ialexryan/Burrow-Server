0486b6fd1735d44c38db1d7c90699a7889cb9c8c
Alex Ryan <ialex.ryan@gmail.com>
2016-04-09 17:03:00 -0700

    Created a run_server script that automatically restarts the server when the source code is changed.

---
+++ run_server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+trap clean_exit SIGINT
+
+function clean_exit() {
+    echo "Shutting down..."
+    pkill -x -f "python server.py"
+    exit 0
+}
+
+while [ 1 ]
+do
+    # start python script and detach
+    echo "Starting python server"
+    python server.py &
+    echo "Python server started as $!"
+    # wait until the script is modified
+    echo "Waiting for the file to be modified"
+    inotifywait -e modify server.py
+    echo "The file was modified!"
+    # send SIGTERM to the script
+    echo "Sending SIGTERM to the script"
+    pgrep -x -f "python server.py" -l
+    kill $!
+    echo "Waiting two seconds..."
+    sleep 2
+    pgrep -x -f "server.py" -l
+done
+
