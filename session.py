a7c12647ec053fe7b604968a465b0af70f0f5feb
Mimi Jiao <tjiao@caltech.edu>
2016-05-02 16:17:02 -0700

    Basic packet forwarding class

--- session.py
+++ session.py
@@ -1,6 +1,35 @@
 import uuid
 
+BUFFER_SIZE = 1024
 sessions = {}
+
+class Forward:
+	sock = None
+	
+	def connect(self, host, port):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			self.sock.connect(host, port)
+			return (True, "s")
+		except Exception as e:
+			return (False, str(e))
+
+	def send(self, message):
+		# Parse out destination and port
+		host = ""
+		port = 8080
+		err = self.connect(host, port)
+		if (err[0] == False):
+			return err
+		self.sock.send(message)
+		return err
+
+	def recieve(self):
+		return self.sock.recv(BUFFER_SIZE)
+
+	def close(self):
+		self.sock.close()
+
 
 def handle_response(message):
 	response = ""
@@ -22,23 +51,27 @@ def handle_response(message):
 
 def send_begin_session():
 	session_id = uuid.uuid4().hex[-8:]
-	sessions[session_id] = ""
+	sessions[session_id] = Forward()
 	return "s-" + str(session_id)
 
 def send_forward_packet(components):
 	session_id = components.next()
 	packet = components.next()
 	# actually forward the packet
-	sessions[session_id] = "I got this: " + packet
-	return "s"
+	err = sessions[session_id].send(packet)
+	if (err[0] == False):
+		return "f-" + err[1] + packet
+	else:
+		return "s"
 
 def send_receive_packet(components):
 	session_id = components.next()
-	data = sessions[session_id]
+	data = sessions[session_id].receive()
 	return "s-" + data
 
 def send_end_session(components):
 	# end the session
 	session_id = components.next()
+	sessions[session_id].close()
 	del sessions[session_id]
 	return "s"
