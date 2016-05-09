d0f1ac7c9434f0f4d3f31e49a5498fa1518e462a
Alex Ryan <ialex.ryan@gmail.com>
2016-05-09 03:17:28 -0700

    Overhaul of sessions, forwarding packets now implemented!

--- session.py
+++ session.py
@@ -1,77 +1,113 @@
 import uuid
 
-BUFFER_SIZE = 1024
+from scapy import route
+from scapy.layers.inet import IP
+from scapy.layers.inet import TCP
+from scapy.layers.inet import UDP
+from scapy.all import sr
+
+SERVER_IP = "131.215.172.230"
+available_ports = range(30000,50000)     #ports will be removed from this list while in use
 sessions = {}
 
-class Forward:
-	sock = None
-	
-	def connect(self, host, port):
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		try:
-			self.sock.connect(host, port)
-			return (True, "s")
-		except Exception as e:
-			return (False, str(e))
+class Session:
+	def __init__(self, id):
+		self.id = id
+		self.pending_response_packets = []
 
-	def send(self, message):
-		# Parse out destination and port
-		host = ""
-		port = 8080
-		err = self.connect(host, port)
-		if (err[0] == False):
-			return err
-		self.sock.send(message)
-		return err
+	def request(self):
+		if len(self.pending_response_packets) == 0:
+			return None
+		else:
+			return self.pending_response_packets.pop(0)
 
-	def recieve(self):
-		return self.sock.recv(BUFFER_SIZE)
+	def forward(self, message):
+		pkt = IP(message)        #parse the binary data to a scapy IP packet
+		print "Forwarding packet"
+		pkt.show2()
 
-	def close(self):
-		self.sock.close()
+		assert(IP in pkt)
+		original_src = pkt[IP].src   #store the original source IP
+		pkt[IP].src = SERVER_IP      #spoof the source IP so the packet comes back to us
+		del pkt[IP].chksum           #invalidate the checksum
+
+		port = available_ports.pop(0)        #get a port from our pool of available ports
+		if TCP in pkt:
+			protocol = TCP
+			original_sport = pkt[TCP].sport  #store the original source port
+			pkt[TCP].sport = port            #spoof the source port
+			#pkt[TCP].dport = ____
+			del pkt[TCP].chksum              #invalidate the checksum
+		elif UDP in pkt:
+			protocol = UDP
+			original_sport = pkt[UDP].sport  #ditto
+			pkt[UDP].sport = port
+			#pkt[UDP].dport = ____
+			del pkt[UDP].chksum
+		else:
+			assert(False)
+
+		pkt = IP(str(pkt))   #recalculate all the checksums
+
+		print "After spoofing, packet looks like:"
+		pkt.show2()
+
+		#send packet and receive responses.
+		#self.ans will contain a list of tuples of sent packets and their responses
+		#self.unans will contain a list of unanswered packets
+		#note: we may need to do some work to make this asynchronous
+		self.ans, self.unans = sr(pkt)
+
+		#un-spoof the source IP address and port,
+		#then add to the list of packets waiting to be sent back
+		for pair in self.ans:
+			response = pair[1]
+			response[IP].src = original_src
+			response[protocol].sport = original_sport
+			self.pending_response_packets.append(str(response))
+
+		available_ports.append(port)  #return port to available pool
 
 
-def handle_response(message):
+def handle_message(message):
 	response = ""
 	components = iter(message.split('-'))
 	type = components.next()
 	if (type == 'b'):
-		response = send_begin_session()
+		response = got_begin_session()
 	elif (type == 'f'):
-		response = send_forward_packet(components)
+		response = got_forward_packet(components)
 	elif (type == 'r'):
-		response = send_receive_packet(components)
+		response = got_request_packet(components)
 	elif (type == 'e'):
-		response = send_end_session(components)
+		response = got_end_session(components)
 	else:
 		# Once we're actually doing packet forwarding, this shouldn't happen
 		# For now, reverse the string
 		response = message[::-1]
 	return response
 
-def send_begin_session():
+def got_begin_session():
 	session_id = uuid.uuid4().hex[-8:]
-	sessions[session_id] = Forward()
+	sessions[session_id] = Session(session_id)
 	return "s-" + str(session_id)
 
-def send_forward_packet(components):
+def got_forward_packet(components):
 	session_id = components.next()
+	session = sessions[session_id]
 	packet = components.next()
-	# actually forward the packet
-	err = sessions[session_id].send(packet)
-	if (err[0] == False):
-		return "f-" + err[1] + packet
+	session.forward(packet)
+	return "s" #we should probably check for failure too
+
+def got_request_packet(components):
+	session_id = components.next()
+	data = sessions[session_id].request()
+	if data == None:
+		return "f-0"
 	else:
-		return "s"
+		return "s-" + data
 
-def send_receive_packet(components):
+def got_end_session(components):
 	session_id = components.next()
-	data = sessions[session_id].receive()
-	return "s-" + data
-
-def send_end_session(components):
-	# end the session
-	session_id = components.next()
-	sessions[session_id].close()
 	del sessions[session_id]
 	return "s"
